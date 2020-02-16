7cae4842440d1f78d2c80d0ae7eb0293dbe3ec43
Alisdair Frost <alisdair.frost@gmail.com>
2020-02-15 19:13:12 -0700

    Simplify testing suite

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -12,36 +12,18 @@ const resultC = 'Crikey, That Was An Interesting One.';
 
 test('capitalizeA - A - Should capitalize string', t => {
   t.is(solutions.capitalizeA(stringA), resultA);
-});
-
-test('capitalizeA - B - Should capitalize string', t => {
   t.is(solutions.capitalizeA(stringB), resultB);
-});
-
-test('capitalizeA - C - Should capitalize string', t => {
   t.is(solutions.capitalizeA(stringC), resultC);
 });
 
 test('capitalizeB - A - Should capitalize string', t => {
   t.is(solutions.capitalizeB(stringA), resultA);
-});
-
-test('capitalizeB - B - Should capitalize string', t => {
   t.is(solutions.capitalizeB(stringB), resultB);
-});
-
-test('capitalizeB - C - Should capitalize string', t => {
   t.is(solutions.capitalizeB(stringC), resultC);
 });
 
 test('capitalizeC - A - Should capitalize string', t => {
   t.is(solutions.capitalizeC(stringA), resultA);
-});
-
-test('capitalizeC - B - Should capitalize string', t => {
   t.is(solutions.capitalizeC(stringB), resultB);
-});
-
-test('capitalizeC - C - Should capitalize string', t => {
   t.is(solutions.capitalizeC(stringC), resultC);
 });
